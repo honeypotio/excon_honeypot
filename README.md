6bad57422734b4c8aca242071c9613b3a285e3b5
Andrzej Trzaska <atrzaska2@gmail.com>
2021-10-04 23:05:33 +0200

    Reuse same methods as excon

--- README.md
+++ README.md
@@ -5,11 +5,12 @@ This gem is a plugin to excon gem https://github.com/excon/excon .
 It adds following functionality:
 
 - default options: expects: [200, 201, 204]
-- `Excon.post_json`, `Excon.put_json` helper methods for requests with json bodies. Json data can be passed with `json` hash param.
+- `Excon.post`, `Excon.put` `json` helper param for requests with json bodies. Json data can be passed with `json` hash param.
 
 Example:
 
-    Excon.post_json('https://foo.bar/users', json: payload)
+    Excon.post('https://foo.bar/users', json: payload)
+    Excon.put('https://foo.bar/users/5', json: payload)
 
 - `response.parsed_body` helper method for easier parsing of json responses
 
--- lib/excon_honeypot.rb
+++ lib/excon_honeypot.rb
@@ -6,19 +6,17 @@ require 'json'
 Excon.defaults[:expects] = [200, 201, 204]
 
 module Excon
-  def self.get_json(url, params = {}, &block)
-    get(url, with_json(params), &block)
+  def self.post(url, params = {}, &block)
+    super(url, with_json(params), &block)
   end
 
-  def self.post_json(url, params = {}, &block)
-    post(url, with_json(params), &block)
-  end
-
-  def self.put_json(url, params = {}, &block)
-    put(url, with_json(params), &block)
+  def self.put(url, params = {}, &block)
+    super(url, with_json(params), &block)
   end
 
   def self.with_json(params)
+    return params if params[:json].nil?
+
     params[:body] ||= JSON.dump(params[:json])
     params[:headers] ||= {}
     params[:headers]['Content-Type'] = 'application/json'
